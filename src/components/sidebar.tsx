87c57b51a771798c66190143d5b4dc53808ee4b6
Ajibo Victor <alexajibo05@gmail.com>
2026-05-20 14:43:48 +0100

    added image to cloud

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -185,7 +185,7 @@ export function Sidebar({ sidebar }: props) {
      
       <div style={headerStyle}>NewsGlobe
         <img
-        src= "src/assets/Globe.jpg"
+        src= "https://res.cloudinary.com/dcspok8u7/image/upload/v1779284549/Globe_io460n.jpg"
         alt= 'Globe Icon'
         style={{ width: '50px', height: '50px' }}>
         </img>
